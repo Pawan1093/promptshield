509e8180915faa5e7ae81e17f2c19bdd696d3364
Pawan Pawar <pawanpawar1093@gmail.com>
2026-08-08 14:39:31 +0530

    feat: add database session management

--- backend/app/configuration/config.py
+++ backend/app/configuration/config.py
@@ -9,6 +9,7 @@ class Settings:
     APP_NAME = os.getenv("APP_NAME")
     APP_VERSION = os.getenv("APP_VERSION")
     DEBUG = os.getenv("DEBUG")
+    DATABASE_URL = os.getenv("DATABASE_URL")
 
 
 settings = Settings()
